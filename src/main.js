a6d9def1dcd7c8e846363a4b23df37fcbacbe01d
jjunyjjuny <jjuny546@gmail.com>
2021-03-29 21:53:58 +0900

    Add webpack-dev-server

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import "./style.scss";
+console.log("successfully rund!!");
+
+const body = document.querySelector("body");
+body.addEventListener("click", ({ target }) => {
+  target.style.backgroundColor = "yellow";
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇddddd");
+});
