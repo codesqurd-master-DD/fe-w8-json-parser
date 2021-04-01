a9416ea564dda2acf388a0bf256946e1733714be
jjunyjjuny <jjuny546@gmail.com>
2021-04-01 23:34:26 +0900

    Refactor parser

--- src/Parser.js
+++ src/Parser.js
@@ -1,10 +1,10 @@
-const tokenizer = require('./Tokenizer.js');
-const lexer = require('./Lexer.js');
+const tokenizer = require("./Tokenizer.js");
+const lexer = require("./Lexer.js");
 const pipe = (...fns) => (arg) => fns.reduce((arg, fn) => fn(arg), arg);
 
 const setOfopenType = {
-  '[': 'array',
-  '{': 'object',
+  "[": "array",
+  "{": "object",
 };
 const string =
   "['1a3',[null,false,['11',[112233],{'easy' : ['hello', {'a':'a', 'b' :'b'}, 'world']},112],55, '99'],{'a':'str', 'b':[912,[5656,33],{'key' : 'inner value', 'newkeys': [1,2,3,4,5]}]}, true, 'a']";
@@ -12,64 +12,77 @@ const string =
 const parser = (list) => {
   const opener = list.shift().value;
   const openType = setOfopenType[opener];
-  if (!openType) throw new Error('not array');
+  if (!openType) throw new Error("not array");
 
   const parentNode = { type: openType, child: [] };
   let prev = [];
   while (list.length > 0) {
-    const { type, value, subType } = list[0];
-    if (isOpen(subType)) parentNode.child.push(parser(list));
-    else if (isClose(subType)) {
-      list.shift();
+    const token = list.shift();
+    const { subType } = token;
+
+    if (isOpen(subType)) {
+      list.unshift(token);
+      parentNode.child.push(parser(list));
+      continue;
+    }
+    if (isClose(subType)) {
       return parentNode;
-    } else {
-      if (isObject(openType)) {
-        let objSet = setObjectProperty();
-        if (prev.length === 0) {
-          prev.push(list.shift());
-        } else if (isProps(subType)) {
-          const key = prev.pop();
-          objSet.value.propKey = { type: key.type, value: key.value };
-          list.shift();
-          const { type, value, subType } = list[0];
+    }
+    if (isArray(openType)) {
+      parentNode.child.push(getElementSet(token));
+      continue;
+    }
+    if (isObject(openType)) {
+      let objSet = setObjectProperty();
+      if (prev.length === 0) {
+        prev.push(token);
+        continue;
+      }
+      if (isColon(subType)) {
+        objSet.value.propKey = createPropKey(prev);
 
-          if (subType) {
-            objSet.value.propValue = parser(list);
-          }
+        const token = list.shift();
+        const { subType } = token;
+        objSet.value.propValue = createPropValue(subType, list, token);
 
-          if (subType === null) {
-            objSet.value.propValue = { type, value };
-            list.shift();
-          }
-          parentNode.child.push(objSet);
-        }
-      } else if (isArray(openType)) {
-        parentNode.child.push({ type, value });
-        list.shift();
+        parentNode.child.push(objSet);
       }
     }
   }
 };
-
+const getElementSet = ({ type, value }) => {
+  return { type, value };
+};
+const createPropKey = (prev) => {
+  const key = prev.pop();
+  return getElementSet(key);
+};
+const createPropValue = (subType, list, token) => {
+  if (subType) {
+    list.unshift(token);
+    return parser(list);
+  } else {
+    return getElementSet(token);
+  }
+};
 const isOpen = (subType) => {
-  return subType === 'open';
+  return subType === "open";
 };
 const isClose = (subType) => {
-  return subType === 'close';
+  return subType === "close";
 };
-const isProps = (subType) => {
-  return subType === 'prop';
+const isColon = (subType) => {
+  return subType === "colon";
 };
 const isObject = (openType) => {
-  return openType === 'object';
+  return openType === "object";
 };
 const isArray = (openType) => {
-  return openType === 'array';
+  return openType === "array";
 };
-
 const setObjectProperty = () => {
   return {
-    type: 'objectProperty',
+    type: "objectProperty",
     value: {
       propKey: {},
       propValue: {},
